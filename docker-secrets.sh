a5473e2c4bfcb9dc6cddc939bfc11d975b6a3add
Jerome Smith <jerome.smith@featurespace.co.uk>
2022-07-07 17:25:24 +0100

    Added docker-creds and ArgoCD manifest

---
+++ docker-secrets.sh
@@ -0,0 +1,1 @@
+kubectl -n argocd create secret docker-registry my-registry-creds --docker-server="https://index.docker.io/v1/" --docker-username="USERNAME" --docker-password="TOKEN" --docker-email="EEEE@MAIL"
